f31ac7cb3207bd9013175e5e8bf190b3a8add60c
PraveenAmujuri <saipraveenamujuri@gmail.com>
2025-12-30 11:39:40 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.PROD
+    ? "https://intelshield.onrender.com"
+    : "http://localhost:8000",
+  withCredentials: false,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
