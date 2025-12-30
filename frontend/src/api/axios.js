c7a574c57339866579b6a99a64c40dd9afa6b335
PraveenAmujuri <saipraveenamujuri@gmail.com>
2025-12-30 21:18:54 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,13 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? "https://intelshield.onrender.com"
-    : "http://localhost:8000",
-  withCredentials: false,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL || 'https://intelshield.onrender.com',
 });
 
 export default api;
